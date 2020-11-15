52d1c587fd563f0ac33eb9306bc75851524dcf51
Roy Golan <rgolan@redhat.com>
2020-11-15 18:25:56 +0200

    Remove unused functions from kubevirt/client
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- pkg/kubevirt/client.go
+++ pkg/kubevirt/client.go
@@ -4,10 +4,7 @@ import (
 	"context"
 	//"time"
 
-	corev1 "k8s.io/api/core/v1"
-	storagev1 "k8s.io/api/storage/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	kubevirtapiv1 "kubevirt.io/client-go/api/v1"
@@ -23,15 +20,9 @@ type ClientBuilderFuncType func(kubeconfig string) (Client, error)
 // Client is a wrapper object for actual infra-cluster clients: kubernetes and the kubevirt
 type Client interface {
 	Ping(ctx context.Context) error
-	GetNamespace(ctx context.Context, name string) (*corev1.Namespace, error)
-	ListNamespace(ctx context.Context) (*corev1.NamespaceList, error)
-	GetStorageClass(ctx context.Context, name string) (*storagev1.StorageClass, error)
 	ListVirtualMachines(namespace string) ([]kubevirtapiv1.VirtualMachineInstance, error)
 	DeleteDataVolume(namespace string, name string) error
 	CreateDataVolume(namespace string, dataVolume *cdiv1alpha1.DataVolume) (*cdiv1alpha1.DataVolume, error)
-	GetDataVolume(namespace string, name string) (*cdiv1alpha1.DataVolume, error)
-	ListDataVolumes(namespace string) ([]cdiv1alpha1.DataVolume, error)
-	GetVMI(ctx context.Context, namespace string, name string) (*kubevirtapiv1.VirtualMachineInstance, error)
 	AddVolumeToVM(namespace string, vmName string, hotPlugRequest *kubevirtapiv1.HotplugVolumeRequest) error
 	RemoveVolumeFromVM(namespace string, vmName string, hotPlugRequest *kubevirtapiv1.HotplugVolumeRequest) error
 }
@@ -83,60 +74,7 @@ func (c *client) Ping(ctx context.Context) error {
 	_, err := c.kubernetesClient.ServerVersion()
 	return err
 }
-func (c *client) GetNamespace(ctx context.Context, name string) (*corev1.Namespace, error) {
-	return c.kubernetesClient.CoreV1().Namespaces().Get(name, metav1.GetOptions{})
-}
-
-func (c *client) ListNamespace(ctx context.Context) (*corev1.NamespaceList, error) {
-	return c.kubernetesClient.CoreV1().Namespaces().List(metav1.ListOptions{})
-}
-
-func (c *client) GetStorageClass(ctx context.Context, name string) (*storagev1.StorageClass, error) {
-	return c.kubernetesClient.StorageV1().StorageClasses().Get(name, metav1.GetOptions{})
-}
-
-// The functions bellow are used for the destroy command
-// Use Dynamic cluster for those actions (list and delete)
-
-func (c *client) ListVirtualMachineInstancesNames(namespace string, requiredLabels map[string]string) ([]string, error) {
-	opts := &metav1.ListOptions{
-		LabelSelector: labels.FormatLabels(requiredLabels),
-	}
-	instanceList, err := c.virtClient.VirtualMachineInstance(namespace).List(opts)
-	if err != nil {
-		return nil, err
-	}
-
-	var names []string
-	for _, vmi := range instanceList.Items {
-		names = append(names, vmi.Name)
-	}
-	return names, nil
-}
 
 func (c *client) DeleteDataVolume(namespace string, name string) error {
 	return c.virtClient.CdiClient().CdiV1alpha1().DataVolumes(namespace).Delete(name, &metav1.DeleteOptions{})
-}
-
-func (c *client) ListDataVolumes(namespace string) ([]cdiv1alpha1.DataVolume, error) {
-	list, err := c.virtClient.CdiClient().CdiV1alpha1().
-		DataVolumes(namespace).
-		List(metav1.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
-	return list.Items, nil
-}
-
-func (c *client) GetDataVolume(namespace string, name string) (*cdiv1alpha1.DataVolume, error) {
-	get, err := c.virtClient.CdiClient().CdiV1alpha1().DataVolumes(namespace).Get(name, metav1.GetOptions{})
-	if err != nil {
-		return nil, err
-	}
-	return get, nil
-}
-
-func (c *client) GetVMI(_ context.Context, namespace string, name string) (*kubevirtapiv1.VirtualMachineInstance, error) {
-	vm, err := c.virtClient.VirtualMachineInstance(namespace).Get(name, &metav1.GetOptions{})
-	return vm, err
 }
