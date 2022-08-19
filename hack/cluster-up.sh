90d5aa3fba0ef28586ff4fb13a44608134e6dfeb
Bartosz Rybacki <brybacki@redhat.com>
2022-08-19 17:48:28 +0200

    WIP: try to update containerd - config
    Signed-off-by: Bartosz Rybacki <brybacki@redhat.com>

--- hack/cluster-up.sh
+++ hack/cluster-up.sh
@@ -23,3 +23,9 @@ echo "Enable hotplug"
 #Add the feature gate to the resource of type Kubevirt.
 ./kubevirtci kubectl patch -n kubevirt kubevirt.kubevirt.io kubevirt  -p '{"spec":  { "configuration": { "developerConfiguration": { "featureGates": ["HotplugVolumes" ] }}}}' -o json --type merge
 
+# TODO: update cluster VMI's with insecure registry...
+# need to run pod per node, and apply change and restart
+# ./kubevirtci kubectl apply -f ./deploy/host-pod.yaml
+# ./kubevirtci kubectl-tenant exec host-pod -- /bin/sh -c "`cat ./deploy/containerd-config.sh`"
+# sudo systemctl restart containerd
+
