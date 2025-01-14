0e7fd3f6169e64040f604deb1acf58aa781d157e
moadqassem <moad.qassem@gmail.com>
2025-01-14 14:57:05 +0100

    support VOLUME_ACCESSIBILITY_CONSTRAINTS capability
    Signed-off-by: moadqassem <moad.qassem@gmail.com>

--- pkg/service/identity.go
+++ pkg/service/identity.go
@@ -55,6 +55,13 @@ func (i *IdentityService) GetPluginCapabilities(context.Context, *csi.GetPluginC
 					},
 				},
 			},
+			{
+				Type: &csi.PluginCapability_Service_{
+					Service: &csi.PluginCapability_Service{
+						Type: csi.PluginCapability_Service_VOLUME_ACCESSIBILITY_CONSTRAINTS,
+					},
+				},
+			},
 		},
 	}, nil
 }
