3b9f81597ca631240a1c7ba86082a65fbd165960
moadqassem <moad.qassem@gmail.com>
2025-01-14 14:58:17 +0100

    propagate the zone/region topologies to the nodes via the CSINode object
    Signed-off-by: moadqassem <moad.qassem@gmail.com>

--- cmd/kubevirt-csi-driver/kubevirt-csi-driver.go
+++ cmd/kubevirt-csi-driver/kubevirt-csi-driver.go
@@ -105,7 +105,11 @@ func handle() {
 		klog.Fatal(err)
 	}
 
-	var nodeID string
+	var (
+		nodeID            string
+		allowedTopologies = map[string]string{}
+	)
+
 	if *nodeName != "" {
 		node, err := tenantClientSet.CoreV1().Nodes().Get(context.TODO(), *nodeName, v1.GetOptions{})
 		if err != nil {
@@ -117,6 +121,11 @@ func handle() {
 			klog.Fatal(err)
 		}
 		klog.Infof("Node name: %v, Node ID: %s", *nodeName, nodeID)
+		// systemUUID is the VM ID
+		nodeID = node.Status.NodeInfo.SystemUUID
+		allowedTopologies[service.WellKnownZoneTopologyKey] = node.Labels[service.WellKnownZoneTopologyKey]
+		allowedTopologies[service.WellKnownRegionTopologyKey] = node.Labels[service.WellKnownRegionTopologyKey]
+		klog.Infof("Node name: %v, Node ID: %s", nodeName, nodeID)
 	}
 
 	identityClientset = tenantClientSet
@@ -134,7 +143,8 @@ func handle() {
 		storageClassEnforcement,
 		nodeID,
 		*runNodeService,
-		*runControllerService)
+		*runControllerService,
+		allowedTopologies)
 
 	driver.Run(*endpoint)
 }
