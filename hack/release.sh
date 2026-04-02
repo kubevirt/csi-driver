3b60f410772e29d711eccd2e7e2d25cbdf2d807c
Alexander Wels <awels@redhat.com>
2026-04-02 13:59:39 -0500

    Add release.sh script so we can make releases of the CSI driver
    Based on the tag in the branch this script will
    create a GH release and upload the yaml needed
    to make the deployment work.
    
    This will work with kustomize based deployments.
    
    Signed-off-by: Alexander Wels <awels@redhat.com>

---
+++ hack/release.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+#Copyright 2026 The csi driver Authors.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+set -exuo pipefail
+
+function cleanup_gh_install() {
+    [ -n "${gh_cli_dir}" ] && [ -d "${gh_cli_dir}" ] && rm -rf "${gh_cli_dir:?}/"
+}
+
+function ensure_gh_cli_installed() {
+    if command -V gh; then
+        return
+    fi
+
+    trap 'cleanup_gh_install' EXIT SIGINT SIGTERM
+
+    # install gh cli for uploading release artifacts, with prompt disabled to enforce non-interactive mode
+    gh_cli_dir=$(mktemp -d)
+    (
+        cd  "$gh_cli_dir/"
+        curl -sSL "https://github.com/cli/cli/releases/download/v${GH_CLI_VERSION}/gh_${GH_CLI_VERSION}_linux_amd64.tar.gz" -o "gh_${GH_CLI_VERSION}_linux_amd64.tar.gz"
+        tar xvf "gh_${GH_CLI_VERSION}_linux_amd64.tar.gz"
+    )
+    export PATH="$gh_cli_dir/gh_${GH_CLI_VERSION}_linux_amd64/bin:$PATH"
+    if ! command -V gh; then
+        echo "gh cli not installed successfully"
+        exit 1
+    fi
+    gh config set prompt disabled
+}
+
+function update_github_release() {
+    # note: for testing purposes we set the target repository, gh cli seems to always automatically choose the
+    # upstream repository automatically, even when you are in a fork
+
+    set +e
+    if ! gh release view --repo "$GITHUB_REPOSITORY" "$TAG" ; then
+        set -e
+        gh release create --repo "$GITHUB_REPOSITORY" "$TAG" --title="$TAG"
+    else
+        set -e
+    fi
+
+    gh release upload --repo "$GITHUB_REPOSITORY" --clobber "$TAG" \
+        $(find deploy -name "*.yaml" -type f)
+}
+
+function main() {
+    TAG="$(git tag --points-at HEAD | head -1)"
+    if [ -z "$TAG" ]; then
+        echo "commit $(git show -s --format=%h) doesn't have a tag, exiting..."
+        exit 0
+    fi
+
+    export TAG
+
+    GIT_ASKPASS="$(pwd)/hack/git-askpass.sh"
+    [ -f "$GIT_ASKPASS" ] || exit 1
+    export GIT_ASKPASS
+
+    ensure_gh_cli_installed
+
+    gh auth login --with-token <"$GITHUB_TOKEN_PATH"
+
+    update_github_release
+}
+
+main "$@"
+
